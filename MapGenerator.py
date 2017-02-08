aafc6f6286deacc8588391ad0dff6bfc2e7eae17
Jakub Kosior <jakubjkosior@gmail.com>
2017-02-08 04:51:07 +0100

    dictionary attribute problem

--- MapGenerator.py
+++ MapGenerator.py
@@ -15,7 +15,7 @@ class Generator(object):
                 continue
             
             check = self.rooms[i]
-            if not ((room.x+room.h < check.x) or (room.x > check.x + check.w) or (room.y + room.h < check.y)
+            if not ((room.x + room.h < check.x) or (room.x > check.x + check.w) or (room.y + room.h < check.y)
                     or (room.y > check.y + check.h)):
                 return True 
             return False
@@ -30,12 +30,11 @@ class Generator(object):
         
         self.data = []
         for i in range (0, self.data_size):
-            self.data[i] = []
+            self.data.append([])
             for j in range (0, self.data_size):
-                self.data[i][j] = None
+                self.data[i].append(None)
                 j += 1
             i += 1
-        
         
         room_count = self.randomizer(10, 20)
         min_size = 5
@@ -49,6 +48,14 @@ class Generator(object):
             room.h = self.randomizer(min_size, max_size)
             i += 1
         
+        return self.data
+    
+
+
+g = Generator()
+print(g.room_gen())
+
+
 
  
         
